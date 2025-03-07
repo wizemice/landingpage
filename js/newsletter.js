865b3f6d0f7dc0524711f8f99fadc4c2c40424ab
joao-gabriel-gois <gois.jg.dev@gmail.com>
2025-03-07 16:23:44 -0300

    feat:mailerLite embedded code w/ centralized captcha + removing overflow-x from body

---
+++ js/newsletter.js
@@ -0,0 +1,30 @@
+(() => {
+  document.addEventListener('DOMContentLoaded', startMutationObserver);
+
+  function startMutationObserver(_) {
+    const targetNode = document.querySelector('[data-form="mhmiBW"]');
+    const config = { attributes: true, childList: true, subtree: true };
+    // console.log("mutation target", targetNode);
+    targetNode.setAttribute('style', 'margin: -1rem 0');
+
+    const callback = (mutationList, observer) => {
+      for (const mutation of mutationList) {
+        if (mutation.type === "childList") {
+          console.log(`The child node ${targetNode} just changed with new newsletter elements added.`);
+          adjustCaptchaToCenter();
+          observer.disconnect();
+        }
+      }
+    }; 
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);  
+  }
+
+  function adjustCaptchaToCenter(_) {
+    const captcha = document.querySelector('.ml-form-recaptcha');
+    if (captcha) {
+      captcha.setAttribute('style', '');
+      captcha.setAttribute('style', 'margin: 0');
+    }
+  }
+})()
